9e76b835cd0dccbbe6b23064fad66ae142c9d228
brGuirra <br.guirra@gmail.com>
2023-11-21 11:06:17 -0300

    feat: add logging for requests and responses

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+
+  app.useLogger(app.get(Logger));
+
+  await app.listen(4000);
 }
-bootstrap();
+
+void bootstrap();
